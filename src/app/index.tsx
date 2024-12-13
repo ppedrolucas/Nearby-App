ca9d2197c0f57450c11a219a61bcd57747e2013f
ppedrolucas <pp15lucas@gmail.com>
2024-12-13 19:08:42 -0300

    navigation home, aula 2

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,8 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { router } from "expo-router";
+
+// Componentes
+import { Button } from "@/components/button";
 import { Steps } from "@/components/steps";
 import { Welcome } from "@/components/welcome";
 
@@ -7,6 +11,9 @@ export default function Index() {
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
       <Steps />
+      <Button onPress={() => router.navigate("/home")}>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   );
 }
